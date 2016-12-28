0cfba33de383d3ee618ab311533669535220f44b
Markus Müller <sukram.mueller@gmail.com>
2016-12-28 01:13:42 +0100

    Added sketchy draft for Eisenhower matrix print

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 
 from tabulate import tabulate
+from itertools import repeat, zip_longest
 
 import tasks_store
 
@@ -25,6 +26,8 @@ def main(argv=sys.argv):
             list_tasks()
     if argv[1] == "stats":
         group_tasks_archived()
+    if argv[1] == "eisenhower":
+        eisenhower_matrix()
     return 0
 
 
@@ -65,6 +68,30 @@ def group_tasks_archived():
     print(tabulate(dates_and_nums,
                    headers=["Date", "Tasks finished"]))
     return dates_and_nums
+
+def eisenhower_matrix():
+    col0 = col1 = col2 = col3 = []
+
+    imp_urg = ["Imp", "Urg"]
+    imp_not_urg = ["Imp", "Not", "Urg"]
+    not_imp_urg = ["Not", "Imp", "Urg"]
+    not_imp_not_urg = ["Not", "Imp", "Not", "Urg"]
+
+    if len(imp_urg) < len(imp_not_urg):
+        diff = len(imp_not_urg) - len(imp_urg)
+        imp_urg.extend([" "] * diff)
+    elif len(imp_not_urg) < len(imp_urg):
+        diff = len(imp_urg) - len(imp_not_urg)
+        imp_urg.extend([" "] * diff)
+
+    col0 = ["Important"] + [" "] * (len(imp_urg) - 1) + ["---", "Not Important"]
+    col1 = imp_urg + ["---"] + not_imp_urg
+    col2 = ["---"] * (len(imp_urg) + 1 + max(len(not_imp_urg), len(not_imp_not_urg)))
+    col3 = imp_not_urg + ["---"] + not_imp_not_urg
+
+    zipped = zip_longest(col0, col1, col2, col3, fillvalue=" ")
+    print(tabulate(list(zipped),
+                   headers=[" ", "Urgent", "---", "Not Urgent"]))
 
 
 def init():
